2db45fbb18f10139ff76b59036fd703f83b6631b
nickd16 <108239710+nickd16@users.noreply.github.com>
2022-06-29 17:50:10 -0500

    Update min_max_alg.py

--- min_max_alg.py
+++ min_max_alg.py
@@ -11,30 +11,44 @@ class min_max_alg():
 
 
     
-    def minimax(FEN, move_list, depth, alpha, beta, current_turn, fn_class):
-        if depth == 0:
-            return f.eval(FEN)
+     if depth == 0:
+            return 0
 
-        alpha_pr = float('inf')
-        beta_pr = float('-inf')
         Game = move_set()
 
         if current_turn:
+            alpha_pr = [float('-inf'), " "]
             for move in move_list:
                 new_fen, new_move_list = Game.update_fen(FEN, move)
-                pr = min_max_alg.minimax(new_fen, new_move_list, depth-1, alpha, beta, False, fn_class)
-                alpha_pr = max(alpha_pr, pr)
-                alpha = max(alpha,pr)
+                pr = min_max_alg.minimax(new_fen, new_move_list, depth -1, alpha, beta, False)
+                if not isinstance(pr, int):
+                    if pr[0] > alpha_pr[0]:
+                        alpha_pr = [pr[0], move]
+                        alpha = max(alpha, pr[0])
+                else:
+                    if pr > alpha_pr[0]:
+                        alpha_pr = [pr, move]
+                        alpha = max(alpha, pr)
                 if beta <= alpha:
                     break
-            return alpha_pr,FEN
+            return alpha_pr
+
         else:
+            beta_pr = [float('inf'), " "]
             for move in move_list:
                 new_fen, new_move_list = Game.update_fen(FEN, move)
-                pr = min_max_alg.minimax(new_fen, new_move_list, depth-1, alpha, beta, True, fn_class)
-                beta_pr = min(beta_pr, pr)
-                beta = min(beta, pr)
-            return beta_pr,FEN
+                pr = min_max_alg.minimax(new_fen, new_move_list, depth -1, alpha, beta, True)
+                if not isinstance(pr, int):
+                    if pr[0] < beta_pr[0]:
+                        beta_pr = [pr[0], move]
+                        beta = min(beta, pr[0])
+                else:
+                    if pr < beta_pr[0]:
+                        beta_pr = [pr, move]
+                        beta = min(beta, pr)
+                if beta <= alpha:
+                    break
+            return beta_pr
             
 
 
